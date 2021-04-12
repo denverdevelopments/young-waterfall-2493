60f6bd299546eea6aabb067f4c85457d93946f6d
kevn7n <kevn_nguyen@yahoo.com>
2021-04-12 10:12:05 -0600

    Create model spec tests, migrations for actor & actor_movie

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActorMovie.destroy_all
+Actor.destroy_all
+Movie.destroy_all
+Studio.destroy_all
+
+universal = Studio.create!(name: 'Universal Studios', location: 'Hollywood')
+focus = Studio.create!(name: 'Focus Films', location: 'London')
+lucas = Studio.create!(name: 'Lucasfilm Ltd', location: 'San Francisco')
+
+raiders = universal.movies.create!(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Adventure')
+princess = focus.movies.create!(title: 'The Princess Bride', creation_year: 1987, genre: 'Romance')
+empire = lucas.movies.create!(title: 'The Empire Strikes Back', creation_year: 1980, genre: 'Adventure')
+jedi = lucas.movies.create!(title: 'Return of the Jedi', creation_year: 1983, genre: 'Adventure')
+
+ford = Actor.create!(name: 'Harrison Ford', age: 78, working: false)
+hamill = Actor.create!(name: 'Mark Hamill', age: 69, working: true)
+elwes = Actor.create!(name: 'Cary Elwes', age: 58, working: true)
+
+ActorMovie.create!(actor: ford, movie: empire)
+ActorMovie.create!(actor: hamill, movie: empire)
+ActorMovie.create!(actor: elwes, movie: princess)
+ActorMovie.create!(actor: ford, movie: princess)
